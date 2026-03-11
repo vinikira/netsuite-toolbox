6a37b29f2df57487099e2566f0e0d3f29c0e9634
Jason Lewis <jason@dickson.st>
2026-03-11 11:39:54 +1100

    exclude login page from injecting to prevent clash with bitwarden keyboard shortcut

--- content-scripts/index.js
+++ content-scripts/index.js
@@ -1,4 +1,14 @@
-if (((document.URL).indexOf('&xml=') === -1) && (!document.getElementById('netsuite-tool-box'))) injectScript()
+const isXml = document.URL.includes('&xml=')
+
+// add explicit check for login page to avoid injecting script
+// and causing errors on login page (and clashes with Bitwarden password manager)
+
+const isLoginPage = window.location.pathname.startsWith('/app/login')
+
+// don't inject on the login page
+if (!isXml && !isLoginPage && !document.getElementById('netsuite-tool-box')) {
+  injectScript()
+}
 
 function onMessage (request, sender, sendResponse) {
     try {
